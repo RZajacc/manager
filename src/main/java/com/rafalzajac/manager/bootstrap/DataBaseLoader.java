fb5518001175bbaf41db540aefd6b9f727ebb2e6
Rafal Zajac <rf.zajac@gmail.com>
2019-03-11 20:35:14 +0100

    slight modification to data base loader

--- src/main/java/com/rafalzajac/manager/bootstrap/DataBaseLoader.java
+++ src/main/java/com/rafalzajac/manager/bootstrap/DataBaseLoader.java
@@ -17,12 +17,11 @@ public class DataBaseLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Player Rafal = new Player((long) 3, "Rafal", "Zajac", "Wing Spiker", 31, 187, 88, 330, 310, "Niebiescy Brzeszcze", "2022");
         Player Marek = new Player((long) 1, "Marek", "Drobisz", "Setter", 34, 189, 103, 315, 305, "Niebiescy Brzeszcze", "2022");
         Player Tomek = new Player((long) 2, "Tomek", "Kulaga", "Wing Spiker", 35, 192, 93, 335, 315, "Unia Bierun", "2022");
-
-        playerRepository.save(Rafal);
+        Player Rafal = new Player((long) 3, "Rafal", "Zajac", "Wing Spiker", 31, 187, 88, 330, 310, "Niebiescy Brzeszcze", "2022");
         playerRepository.save(Marek);
         playerRepository.save(Tomek);
+        playerRepository.save(Rafal);
     }
 }
